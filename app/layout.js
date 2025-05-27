2f3c8404a933e816ac133c660bb66e304e2fa288
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-05-27 12:11:26 +0700

    Update : rooting

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import Navigation from './_components/Navigation';
-import Logo from './_components/Logo';
+import Navigation from '@/app/_components/Navigation';
+import Logo from '@/app/_components/Logo';
 
 export const metadata = {
   title: 'The Wild Oasis',
