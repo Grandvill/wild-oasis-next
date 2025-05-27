b4507e2b2572c7fe9f66300d3765358da6bd3acd
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-05-27 14:19:09 +0700

    Update : improving the navigation and root layout

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Header from '@/app/_components/Header';
 import Navigation from '@/app/_components/Navigation';
 import Logo from '@/app/_components/Logo';
 import { Josefin_Sans } from 'next/font/google';
@@ -20,13 +21,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+        <Header />
+        <div className="flex-1 px-8 py-12 ">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
