8d81afa4407af84965364c983cff3653821b7e5d
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-05-30 11:34:38 +0700

    Update : streaming ui with suspense: cabin list

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,12 @@
-import CabinCard from '@/app/_components/CabinCard';
-import { getCabins } from '@/app/_lib/data-service';
+import { Suspense } from 'react';
+import CabinList from '../_components/CabinList';
+import Spinner from '../_components/Spinner';
 
 export const metadata = {
   title: 'Cabins',
 };
 
-export default async function Page() {
-  const cabins = await getCabins();
-
+export default function Page() {
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">Our Luxury Cabins</h1>
@@ -16,13 +15,10 @@ export default async function Page() {
         stars. Enjoy nature&apos;s beauty in your own little home away from home. The perfect spot for a peaceful, calm vacation. Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      {/* suspense berfungsi untuk menampilkan loading pada komponen tertentu, tidak semua halaman membutuhkan loading */}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
