a616f613d94a89d7a84034e0367ec68ce4843f65
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-06-02 13:06:19 +0700

    Add : text expander component

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,6 +1,7 @@
 import { getCabin, getCabins } from '@/app/_lib/data-service';
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
+import TextExpander from '../../_components/TextExpander';
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
@@ -33,7 +34,9 @@ export default async function Page({ params }) {
         <div>
           <h3 className="text-accent-100 font-black text-7xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]">Cabin {name}</h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
