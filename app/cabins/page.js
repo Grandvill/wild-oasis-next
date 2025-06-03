54d55aff7f9a4a628cdd34e8e554328f8361a3e9
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-06-03 13:23:42 +0700

    Add : filter in cabin page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import CabinList from '../_components/CabinList';
 import Spinner from '../_components/Spinner';
+import Filter from '../_components/Filter';
 
 // export const revalidate = 0;
 
@@ -8,7 +9,8 @@ export const metadata = {
   title: 'Cabins',
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? 'all';
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">Our Luxury Cabins</h1>
@@ -17,9 +19,13 @@ export default function Page() {
         stars. Enjoy nature&apos;s beauty in your own little home away from home. The perfect spot for a peaceful, calm vacation. Welcome to paradise.
       </p>
 
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
       {/* suspense berfungsi untuk menampilkan loading pada komponen tertentu, tidak semua halaman membutuhkan loading */}
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
