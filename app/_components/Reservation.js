c0ac9e7c4d32e5775b902c047c8cf70c8681f277
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-06-13 20:38:55 +0700

    Update : Calendar styling

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -5,7 +5,7 @@ import { getSettings, getBookedDatesByCabinId } from '../_lib/data-service';
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)]);
   return (
-    <div className="flex justify-center items-end border border-primary-800 min-h-[400px]">
+    <div className="flex flex-row justify-between items-start border border-primary-800 min-h-[400px] w-full max-w-6xl mx-auto">
       <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
       <ReservationForm cabin={cabin} />
     </div>
