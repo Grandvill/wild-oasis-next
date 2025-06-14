383fd8db419ec3d7c7bafe93a77bcd9ec0675293
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-06-14 18:23:01 +0700

    Add : context api for date selector and reservation form component

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,11 +1,12 @@
+import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service';
 import DateSelector from './DateSelector';
 import ReservationForm from './ReservationForm';
-import { getSettings, getBookedDatesByCabinId } from '../_lib/data-service';
 
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)]);
+
   return (
-    <div className="flex flex-row justify-between items-start border border-primary-800 min-h-[400px] w-full max-w-6xl mx-auto">
+    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
       <ReservationForm cabin={cabin} />
     </div>
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import CabinList from '../_components/CabinList';
 import Spinner from '../_components/Spinner';
 import Filter from '../_components/Filter';
+import ReservationReminder from '../_components/ReservationReminder';
 
 // export const revalidate = 0;
 
@@ -26,6 +27,7 @@ export default function Page({ searchParams }) {
       {/* suspense berfungsi untuk menampilkan loading pada komponen tertentu, tidak semua halaman membutuhkan loading */}
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
