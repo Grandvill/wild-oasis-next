5aee8145c1412d598b6d92293cc78104f6a7a896
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-06-24 14:15:34 +0700

    Update : loading indicator in update profile button using useFormStatus hook

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -2,14 +2,12 @@
 
 import { useState } from 'react';
 import { updateGuest } from '../_lib/actions';
+import { useFormStatus } from 'react-dom';
 
 function UpdateProfileForm({ guest, children }) {
   const [count, setCount] = useState();
 
   const { fullName, email, nationality, nationalID, countryFlag } = guest;
-
-  // const countryFlag = 'pt.jpg';
-  // const nationality = 'portugal';
 
   return (
     <form action={updateGuest} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
@@ -35,11 +33,20 @@ function UpdateProfileForm({ guest, children }) {
         <label htmlFor="nationalID">National ID number</label>
         <input defaultValue={nationalID} name="nationalID" className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm" />
       </div>
-
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">Update profile</button>
+        <Button />
       </div>
     </form>
+  );
+}
+
+function Button() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300" disabled={pending}>
+      {pending ? 'Updating...' : 'Update Profile'}
+    </button>
   );
 }
 
