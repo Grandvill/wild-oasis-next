857ef877f7215e04dbf6cc551731f9aefa669166
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-06-26 14:58:44 +0700

    Updating a reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './Auth';
 import { supabase } from './supabase';
 import { getBookings } from './data-service';
+import { redirect } from 'next/navigation';
 
 // action.js : file ini digunakan untuk logic yang berkaitan dengan session, validasi user, dan revalidate cache setelah operasi data
 export async function updateGuest(formData) {
@@ -26,7 +27,7 @@ export async function updateGuest(formData) {
 
   if (error) throw new Error('Guest could not be updated');
 
-  // revalidate berfungsi untuk memperbarui cache halaman ini tanpa perlu refresh browser
+  // revalidate berfungsi untuk memperbarui cache halaman ini tanpa perlu refresh browser / biar ga ada cache lama yang masih tersimpan
   revalidatePath('/account/profile');
 }
 
@@ -45,6 +46,39 @@ export async function deleteReservation(bookingId) {
     throw new Error('Booking could not be deleted');
   }
   revalidatePath('/account/reservations');
+}
+
+export async function updateReservation(formData) {
+  const bookingId = Number(formData.get('bookingId'));
+
+  // 1. Authentication: check if user is logged in
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in to update a reservation.');
+
+  // 2. Authorization: check if the booking belongs to the logged-in user
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsIds.includes(bookingId)) throw new Error('You are not allowed to update this booking.');
+
+  // 3. Buidling the update data
+  const updateData = {
+    numGuests: Number(formData.get('numGuests')),
+    observations: formData.get('observations').slice(0, 100), // ambil hanya 100 karakter pertama
+  };
+
+  // 4. Mutation : update the booking
+  const { error } = await supabase.from('bookings').update(updateData).eq('id', bookingId).select().single();
+
+  // 5. Error handling
+  if (error) throw new Error('Booking could not be updated');
+
+  // 6. revalidate the reservations page to reflect the changes / biar ga ada cache lama yang masih tersimpan
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath('/account/reservations');
+
+  // 7. redirecting the user to the reservations page
+  redirect(`/account/reservations`);
 }
 
 export async function signInAction() {
