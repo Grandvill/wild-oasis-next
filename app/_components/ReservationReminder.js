00539d82b0e142287a86832fa6e96617531f6ca5
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-07-15 12:46:02 +0700

    Fix : special character single quotes

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -19,7 +19,7 @@ function ReservationReminder() {
   return (
     <div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-accent-500 text-primary-800 text font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center">
       <p>
-        <span>👋</span> Don't forget to reserve your dates <br /> from {format(fromDate, 'MMM dd yyyy')} to {format(toDate, 'MMM dd yyyy')}
+        <span>👋</span> Don&apos;t forget to reserve your dates <br /> from {format(fromDate, 'MMM dd yyyy')} to {format(toDate, 'MMM dd yyyy')}
       </p>
       <button className="rounded-full p-1 hover:bg-accent-600 transition-all" onClick={resetRange}>
         <XMarkIcon className="h-5 w-5" />
